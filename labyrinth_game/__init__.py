bf23ed6ac9d78217429ca881544524acccab8c78
An <6878811@mail.ru>
2026-01-02 16:47:53 +0300

    Update __init__.py

---
+++ labyrinth_game/__init__.py
@@ -0,0 +1,3 @@
+from .constants import ROOMS, START_ROOM, WIN_ROOM, COMMANDS, MESSAGES
+from .player_actions import move_player, take_item, use_item, look_items, get_input
+from .utils import describe_room
